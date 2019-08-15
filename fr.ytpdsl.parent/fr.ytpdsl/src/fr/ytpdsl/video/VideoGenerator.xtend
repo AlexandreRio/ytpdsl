1cd34ed4d5f4621f6c86a27850a9d11a81f6df34
Alexandre Rio <contact@alexrio.fr>
2019-08-15 18:29:17 +0200

    better manage the dependencies using a third-party OSGi bundle bridging OSGi and maven world

--- fr.ytpdsl.parent/fr.ytpdsl/src/fr/ytpdsl/video/VideoGenerator.xtend
+++ fr.ytpdsl.parent/fr.ytpdsl/src/fr/ytpdsl/video/VideoGenerator.xtend
@@ -6,15 +6,20 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.List
+import java.util.stream.Collectors
+import net.bramp.ffmpeg.FFprobe
 
 class VideoGenerator {
 
+	var static FFprobe ffprobe
+
 	def static generate(YtpModel root) {
+		ffprobe = new FFprobe("/path/to/ffprobe");
 		loadLibraries(root.information.library)
 	}
 
 	def static loadLibraries(List<VideoLibrary> libraries) {
-		libraries.forEach(lib|scanLibrary(lib))
+		libraries.forEach[lib|scanLibrary(lib)]
 	}
 
 	def static scanLibrary(VideoLibrary lib) {
@@ -23,13 +28,11 @@ class VideoGenerator {
 		val res = Files.find(Paths.get(folder.toURI),
 			Integer.MAX_VALUE,
 			[path, attr | lib.extension.stream.anyMatch(ext | path.fileName.toString.endsWith(ext))])
-		.count
-		
-		println(res + " files found")
+		.collect(Collectors.toList)
+
+		res.forEach[video | ]
+		println(res.size + " files found")
 
 	}
 
-	def recursiveScan(File f) {
-	}
-	
 }
--- fr.ytpdsl.parent/fr.ytpdsl/xtend-gen/fr/ytpdsl/video/VideoGenerator.java
+++ fr.ytpdsl.parent/fr.ytpdsl/xtend-gen/fr/ytpdsl/video/VideoGenerator.java
@@ -11,13 +11,23 @@ import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import net.bramp.ffmpeg.FFprobe;
 import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 
 @SuppressWarnings("all")
 public class VideoGenerator {
+  private static FFprobe ffprobe;
+  
   public static void generate(final YtpModel root) {
-    VideoGenerator.loadLibraries(root.getInformation().getLibrary());
+    try {
+      FFprobe _fFprobe = new FFprobe("/path/to/ffprobe");
+      VideoGenerator.ffprobe = _fFprobe;
+      VideoGenerator.loadLibraries(root.getInformation().getLibrary());
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
   
   public static void loadLibraries(final List<VideoLibrary> libraries) {
@@ -40,18 +50,18 @@ public class VideoGenerator {
           };
           return lib.getExtension().stream().anyMatch(_function_1);
         };
-        final long res = Files.find(Paths.get(folder.toURI()), 
-          Integer.MAX_VALUE, _function).count();
-        String _plus = (Long.valueOf(res) + " files found");
+        final List<Path> res = Files.find(Paths.get(folder.toURI()), 
+          Integer.MAX_VALUE, _function).collect(Collectors.<Path>toList());
+        final Consumer<Path> _function_1 = (Path video) -> {
+        };
+        res.forEach(_function_1);
+        int _size = res.size();
+        String _plus = (Integer.valueOf(_size) + " files found");
         _xblockexpression = InputOutput.<String>println(_plus);
       }
       return _xblockexpression;
     } catch (Throwable _e) {
       throw Exceptions.sneakyThrow(_e);
     }
-  }
-  
-  public Object recursiveScan(final File f) {
-    return null;
   }
 }
