30964d0deb0b8d4005f73261a3c9cae618c515aa
Alexandre Rio <contact@alexrio.fr>
2019-09-07 22:37:05 +0200

    cut videos, now try to merge them

--- fr.ytpdsl.parent/fr.ytpdsl.ui/src/fr/ytpdsl/ui/handlers/GeneratorHandler.java
+++ fr.ytpdsl.parent/fr.ytpdsl.ui/src/fr/ytpdsl/ui/handlers/GeneratorHandler.java
@@ -1,15 +1,26 @@
 package fr.ytpdsl.ui.handlers;
 
+import java.util.concurrent.TimeUnit;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.progress.IProgressConstants;
 
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -19,37 +30,98 @@ import fr.ytpdsl.YtpDslStandaloneSetup;
 import fr.ytpdsl.ui.YtpDSLConsole;
 import fr.ytpdsl.video.VideoGenerator;
 import fr.ytpdsl.ytpDsl.YtpModel;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFmpegUtils;
+import net.bramp.ffmpeg.progress.Progress;
+import net.bramp.ffmpeg.progress.ProgressListener;
 
 public class GeneratorHandler extends AbstractHandler {
 
 	@Inject
 	private Provider<ResourceSet> resourceSetProvider;
 
+	private YtpDSLConsole console = YtpDSLConsole.getInstance();
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+
 		IWorkbenchPart workbenchPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
 				.getActivePart();
 		IFile file = (IFile) workbenchPart.getSite().getPage().getActiveEditor().getEditorInput()
 				.getAdapter(IFile.class);
+		IFolder destFolder = file.getProject().getFolder("src-gen");
+		if (!destFolder.exists()) {
+			try {
+				destFolder.create(true, true, null);
+			} catch (CoreException e) {
+				console.printErrorln("Cannot create destination folder : " + e.getMessage());
+			}
+		}
 
 		Injector injector = new YtpDslStandaloneSetup().createInjectorAndDoEMFRegistration();
 		GeneratorHandler handler = injector.getInstance(GeneratorHandler.class);
-		handler.run(file.getLocation().toString());
+		handler.run(file.getLocation().toString(), destFolder.getLocation().toString());
 		return null;
 	}
 	
-	public void run(String input) {
+	
+	public void run(String inputModelPath, String outputDirPath) {
 		YtpDSLConsole console = YtpDSLConsole.getInstance();
 
 		ResourceSet set = resourceSetProvider.get();
-
-		Resource resource = set.getResource(URI.createFileURI(input), true);
+		Resource resource = set.getResource(URI.createFileURI(inputModelPath), true);
 		EObject root = resource.getContents().get(0);
 		if (root instanceof YtpModel) {
 			console.printMessageln("Generating a video...");
-			VideoGenerator generator = new VideoGenerator((YtpModel) root);
-			console.printMessageln(generator.toString());
+
+			Job job = new Job("Generating a video") {
+				protected IStatus run(IProgressMonitor monitor) {
+					monitor.beginTask("fait des trucs", 100);
+					VideoGenerator generator = new VideoGenerator((YtpModel) root, outputDirPath);
+					FFmpegExecutor executor = new FFmpegExecutor(generator.ffmpeg, generator.ffprobe);
+					executor.createJob(generator.build().done(), new ProgressListener() {
+						private int lastPercentage = 0;
+
+						@Override
+						public void progress(Progress progress) {
+							double percentage = progress.out_time_ns / generator.duration();
+							int roundedPercentage = ((int)percentage*100);
+							if (lastPercentage != roundedPercentage) {
+								monitor.worked(roundedPercentage - lastPercentage);
+								lastPercentage =  roundedPercentage;
+							}
+
+							console.printMessageln(String.format(
+									"[%.0f%%] status:%s frame:%d time:%s ms fps:%.0f speed:%.2fx",
+									percentage * 100,
+									progress.status,
+									progress.frame,
+									FFmpegUtils.toTimecode(progress.out_time_ns, TimeUnit.NANOSECONDS),
+									progress.fps.doubleValue(),
+									progress.speed
+								));
+						}}
+					).run();
+					
+					setProperty(IProgressConstants.KEEP_PROPERTY, Boolean.TRUE);
+					setProperty(IProgressConstants.ACTION_PROPERTY, displayResultAction());
+					return Status.OK_STATUS;
+				}
+
+				protected Action displayResultAction() {
+					return new Action("View reservation status") {
+						public void run() {
+							MessageDialog.openInformation(
+									PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+									"Reservation Complete", "Your reservation has been ");
+						}
+					};
+				}
+			};
+			job.setUser(true);
+			job.setPriority(Job.LONG);
+			job.schedule(); // start as soon as possible
+
 		}
 	}
 }
--- fr.ytpdsl.parent/fr.ytpdsl/src/fr/ytpdsl/video/VideoGenerator.xtend
+++ fr.ytpdsl.parent/fr.ytpdsl/src/fr/ytpdsl/video/VideoGenerator.xtend
@@ -4,41 +4,72 @@ import fr.ytpdsl.ytpDsl.VideoLibrary
 import fr.ytpdsl.ytpDsl.YtpModel
 import java.io.File
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.List
+import java.util.concurrent.TimeUnit
 import java.util.stream.Collectors
 import net.bramp.ffmpeg.FFmpeg
 import net.bramp.ffmpeg.FFprobe
-import java.nio.file.Path
+import net.bramp.ffmpeg.builder.FFmpegBuilder
+import java.util.UUID
 
 class VideoGenerator {
 
-	var FFprobe ffprobe
-	var FFmpeg ffmpeg
+	public var FFprobe ffprobe
+	public var FFmpeg ffmpeg
 
 	var List<Path> mediaList
 
-	new(YtpModel root) {
+	var String destinationFolder
+
+	var String video
+
+	new(YtpModel root, String destinationDirectory) {
 		ffprobe = new FFprobe(root.information.ffprobe)
 		ffmpeg = new FFmpeg(root.information.ffmpeg)
 		mediaList = loadLibraries(root.information.library)
+
+		destinationFolder = destinationDirectory
 	}
 
-	private def generate() {
-		mediaList.forEach[video | println(video + " " + ffprobe.probe(video.toString).format.duration) ]
+
+	def duration() {
+		ffprobe.probe(video).format.duration * TimeUnit.SECONDS.toNanos(1)
+	}
+
+	def concat() {
+		
+	}
+
+	def build() {
+		var builder = new FFmpegBuilder()
+		builder.input = destinationFolder + "/pl.list"
+		builder.format = "concat"
+		builder.addExtraArgs("-safe", "0")
+		builder.addOutput(destinationFolder + "/sortie.mp4")
+//		video = mediaList.get((Math.random * mediaList.length) as int).toString
+//	
+//		val du = duration()
+//		var builder = new FFmpegBuilder();
+//		builder.input = video
+//
+//		builder.setStartOffset( (du as int )/ 2 as long, TimeUnit.NANOSECONDS)
+//		println("total duration " + du)
+//		println( (du as int )/ 2 as long + " ns from start for 5s")
+//		builder.addOutput(destinationFolder + "/" + UUID.randomUUID + ".mp4").setDuration(5, TimeUnit.SECONDS)
 	}
 
 	private def loadLibraries(List<VideoLibrary> libraries) {
-		libraries.map[lib | scanLibrary(lib)].stream.flatMap[l | l.stream].collect(Collectors.toList)
+		libraries.map[lib|scanLibrary(lib)].stream.flatMap[l|l.stream].collect(Collectors.toList)
 	}
 
 	private def scanLibrary(VideoLibrary lib) {
 		val folder = new File(lib.folder)
-		print("Scanning " + folder + " ")
-		val res = Files.find(Paths.get(folder.toURI),
-			Integer.MAX_VALUE,
-			[path, attr | lib.extension.stream.anyMatch(ext | path.fileName.toString.endsWith(ext))])
-		.collect(Collectors.toList)
+		println("Scanning " + folder + " ")
+		val res = Files.find(Paths.get(folder.toURI), Integer.MAX_VALUE, [ path, attr |
+			lib.extension.stream.anyMatch(ext|path.fileName.toString.endsWith(ext))
+		]).collect(Collectors.toList)
 
 		res
 	}
@@ -46,7 +77,7 @@ class VideoGenerator {
 	override toString() {
 		var debugString = ffmpeg.version + "\n" + ffprobe.version + "\n"
 		debugString += mediaList.size + " videos indexed"
-		debugString	
+		debugString
 	}
-	
+
 }
